21ac0b1090aa226d2dd9fe705875f84e782791b1
Olof Thoren <ollethoren@hotmail.com>
2026-04-10 21:17:57 +0200

    Add sitemap and robots.txt

---
+++ app/src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.fundbridge.se",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.fundbridge.se/om-fundbridge",
+    },
+    {
+      url: "https://www.fundbridge.se/kontakt",
+    },
+    {
+      url: "https://www.fundbridge.se/soka-stipendium",
+    },
+    {
+      url: "https://www.fundbridge.se/stipendier-sverige",
+    },
+    {
+      url: "https://www.fundbridge.se/stipendier-studenter",
+    },
+  ];
+}
